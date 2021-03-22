baa4a63bdc5896b4bb9f90926c210613d37d980f
João <joao.vigoraes@gmail.com>
2021-03-22 19:46:17 -0300

    adding the database

--- pdo/views/partials/nav.php
+++ pdo/views/partials/nav.php
@@ -2,7 +2,6 @@
 	<ul>
 		<li><a href="/">Home</a></li>
 		<li><a href="/about">About</a></li>
-		<li><a href="/about/culture">About Our Culture</a></li>
 		<li><a href="/contact">Contact</a></li>
 	</ul>
 </nav>
